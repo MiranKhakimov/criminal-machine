dc0b2dea9bdacfaba1fe829c10e5de91d5f6b8dd
George050 <IStormer8@gmail.com>
2023-07-06 18:35:40 +0500

    Text FireBall Add
    Press P

--- Assets/Scripts/attack_input.cs
+++ Assets/Scripts/attack_input.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class attack_input : MonoBehaviour
 {
     List<List<int>> input_matrix = new List<List<int>> {new List<int> {7, 8, 9}, new List<int> {4, 5, 6}, new List<int> {1, 2, 3}};
-    List<int> frames_input = Enumerable.Repeat(5, 20).ToList();
+    List<int> frames_input = Enumerable.Repeat(5, 50).ToList();
+    List<int> input_for_cast;
     int x, y;
     // Start is called before the first frame update
     void Start()
@@ -21,24 +23,37 @@ public class attack_input : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            List<int> input_for_cast = new List<int>(frames_input[0]);
-            for (int i = 1; i < frames_input.Count - 1; i++)
-            {
-                if (frames_input[i] != 5)
-                {
-                    if (frames_input[i] != frames_input[i - 1])
-                    {
-                        input_for_cast.Add(frames_input[i]);
-                    }
-                }
-            }
+            Input_Update();
             Debug.Log(string.Join(",", input_for_cast.ToArray()));
             Debug.Log(string.Join(",", frames_input.ToArray()));
+            for (int i = 0; i < input_for_cast.Capacity - 2; i++) 
+            {
+                if (IsSublist(input_for_cast, new List<int>{2, 1, 4}) || IsSublist(input_for_cast, new List<int>{2, 3, 6}))
+                {
+                    Debug.Log("FIREBALL");
+                    break;
+                }
+            }
         }
-        
     }
 
-    private void FixedUpdate()
+    void Input_Update() 
+    {
+        input_for_cast = new List<int>();
+        input_for_cast.Add(frames_input[0]);
+        for (int i = 1; i < frames_input.Count - 1; i++)
+        {
+            if (frames_input[i] != 5)
+            {
+                if (frames_input[i] != frames_input[i - 1])
+                {
+                    input_for_cast.Add(frames_input[i]);
+                }
+            }
+        }
+    }
+
+    void FixedUpdate()
     {
         x = 1;
         y = 1;
@@ -62,5 +77,22 @@ public class attack_input : MonoBehaviour
         frames_input.RemoveAt(0); 
         frames_input.Add(input_matrix[y][x]);
 
+    }
+
+    static bool IsSublist<T>(IEnumerable<T> list1, IEnumerable<T> list2)
+    {
+        int count1 = list1.Count();
+        int count2 = list2.Count();
+
+        if (count2 > count1)
+            return false;
+
+        for (int i = 0; i <= count1 - count2; i++)
+        {
+            if (Enumerable.Range(i, count2).Select(index => list1.ElementAt(index)).SequenceEqual(list2))
+                return true;
+        }
+
+        return false;
     }
 }
